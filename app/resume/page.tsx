e957b6b1aa6c1df9a4adad02c31e57f17935ba5c
Aaradhya <aaradhya.shek@gmail.com>
2026-07-26 21:46:47 +0530

    Link error fixed again

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation';
 
-const resumeUrl = 'https://drive.google.com/file/d/1QrkmofRt1A3AQajgdcGb3b5Vzub-OI5R/view?usp=drive_link';
+const resumeUrl = 'https://drive.google.com/file/d/1QrkmofRt1A3AQajgdcGb3b5Vzub-OI5R/view';
 export default function ResumePage() {
   redirect(resumeUrl);
 }
